26a5cfb59af4ccc2f5aa969344e9bca3e2fe46b3
Administrator <Administrator@AFOGL-703071853>
2017-12-27 08:56:03 +0800

    添加ThreadStatic

---
+++ AsyncAndThreading/多线程/多线程静态.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace AsyncAndThreading.多线程
+{
+    static class 多线程静态
+    {
+        [ThreadStatic]
+        static int i = 3;
+
+        public static void ChangePrintVal(string from)
+        {
+            i++;
+            Console.WriteLine(from + ", i = " + i);
+        }
+
+        public static void Exec()
+        {
+            var t1 = new Thread(() =>
+            {
+                ChangePrintVal("t1's tid: " + Thread.CurrentThread.ManagedThreadId);
+            });
+            t1.Start();
+
+            var t2 = new System.Threading.Thread(() =>
+            {
+                ChangePrintVal("t2's tid: " + Thread.CurrentThread.ManagedThreadId);
+            });
+            t2.Start();
+        }
+    }
+}

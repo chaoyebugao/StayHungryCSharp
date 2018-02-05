979e3113c25a8d2d850697da07383ba9c4972bbe
Administrator <Administrator@AFOGL-703071853>
2018-02-05 14:47:25 +0800

    设计模式 - 单例

---
+++ ConsoleApp1/单例/Singleton.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.单例
+{
+
+    /// <summary>
+    /// 单例模式
+    /// 确保一个类只有一个实例,并提供一个全局访问点
+    /// </summary>
+    public class Singleton
+    {
+        private static Singleton uniqueInstance;
+
+        private static readonly object syncObj = new object();
+
+        /// <summary>
+        /// 私有掉构造方法，使<see cref="Singleton"/>类不能被外部创建
+        /// </summary>
+        private Singleton()
+        {
+
+        }
+
+
+        /// <summary>
+        /// 定义外部访问点来提供单例
+        /// </summary>
+        /// <returns></returns>
+        public static Singleton GetSingleton()
+        {
+            if (uniqueInstance == null)
+            {
+                //双重锁定，外部的锁定是为了避免每次都lock，if可以避免这样不必要的性能损失
+                lock (syncObj)
+                {
+                    if(uniqueInstance == null)
+                    {
+                        uniqueInstance = new Singleton();
+                    }
+                }
+            }
+
+            return uniqueInstance;
+        }
+    }
+}
---
+++ ConsoleApp1/简单工厂/SimpleFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.简单工厂
+{
+    /// <summary>
+    /// 简单工厂模式
+    /// 负责生产对象的一个类
+    /// </summary>
+    class SimpleFactory
+    {
+    }
+}
